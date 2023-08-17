eb765c4b922d4ca22400a1c8f507c1358f4f8277
Marcus Reis <marcus.reis@askblue.pt>
2023-08-17 22:02:07 +0100

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
+import Navbar from './components/Navbar'
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
@@ -19,6 +20,7 @@ function App() {
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
       </Routes>
+      <Navbar />
     </Router>
     </>
   );
