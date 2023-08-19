2d7042822d9d81cd0b912b70d6f5f558c248d546
Marcus Reis <marcus.reis@askblue.pt>
2023-08-19 20:31:59 +0100

    Start Create Listing Page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,19 @@
-import {useState } from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { getAuth, updateProfile } from 'firebase/auth'
 import { doc, updateDoc } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify';
+import { toast } from 'react-toastify'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 
 function Profile() {
   const auth = getAuth()
   const [changeDetails, setChangeDetails] = useState(false)
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
-    email: auth. currentUser.email,
+    email: auth.currentUser.email,
   })
 
   const { name, email } = formData
@@ -22,16 +25,16 @@ function Profile() {
 
   const onSubmit = async () => {
     try {
-      if(auth.currentUser.displayName !== name){
+      if (auth.currentUser.displayName !== name) {
         // Update display name in fb
-        await updateProfile(auth.currentUser, { 
-          displayName: name
+        await updateProfile(auth.currentUser, {
+          displayName: name,
         })
 
         // Update in firestore
         const userRef = doc(db, 'users', auth.currentUser.uid)
         await updateDoc(userRef, {
-          name
+          name,
         })
       }
     } catch (error) {
@@ -48,51 +51,57 @@ function Profile() {
   }
 
   return (
-    <div className="profile">
-      <header className="profileHead">
-        <p className="pageHeader">My Profile</p>
-        <button type="button" className="logOut" onClick={onLogout}>
+    <div className='profile'>
+      <header className='profileHead'>
+        <p className='pageHeader'>My Profile</p>
+        <button type='button' className='logOut' onClick={onLogout}>
           Logout
         </button>
       </header>
 
       <main>
-        <div className="profileDetailsHeader">
-          <p className="personalDetailsText">Personal Details</p>
+        <div className='profileDetailsHeader'>
+          <p className='personalDetailsText'>Personal Details</p>
           <p
-            className="changePersonalDetails"
+            className='changePersonalDetails'
             onClick={() => {
-              changeDetails && onSubmit();
-              setChangeDetails((prevState) => !prevState);
+              changeDetails && onSubmit()
+              setChangeDetails((prevState) => !prevState)
             }}
           >
-            {changeDetails ? "done" : "change"}
+            {changeDetails ? 'done' : 'change'}
           </p>
         </div>
 
-        <div className="profileCard">
+        <div className='profileCard'>
           <form>
             <input
-              type="text"
-              id="name"
-              className={!changeDetails ? "profileName" : "profileNameActive"}
+              type='text'
+              id='name'
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
               disabled={!changeDetails}
               value={name}
               onChange={onChange}
             />
             <input
-              type="text"
-              id="email"
-              className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+              type='text'
+              id='email'
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
               disabled={!changeDetails}
               value={email}
               onChange={onChange}
             />
           </form>
         </div>
+
+        <Link to='/create-listing' className='createListing'>
+          <img src={homeIcon} alt='home' />
+          <p>Sell or rent your home</p>
+          <img src={arrowRight} alt='arrow right' />
+        </Link>
       </main>
     </div>
-  );
+  )
 }
 
 export default Profile
