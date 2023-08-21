f4bdac59fd05987abceb8ab7459844d7e0a296a4
Marcus Reis <marcus.reis@askblue.pt>
2023-08-21 23:27:45 +0100

    Profile Listings & Delete

--- src/pages/CreatingListing.jsx
+++ src/pages/CreatingListing.jsx
@@ -56,7 +56,7 @@ function CreatingListing() {
     if (isMounted) {
       onAuthStateChanged(auth, (user) => {
         if (user) {
-          setFormData({ ...formData, useRef: user.uid })
+          setFormData({ ...formData, userRef: user.uid })
         } else {
           navigate('/sign-in')
         }
