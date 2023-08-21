09a894d2cb7c71069b90d074d636b210b8ca133e
Marcus Reis <marcus.reis@askblue.pt>
2023-08-22 00:17:22 +0100

    Edit Listing Icon

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -106,6 +106,8 @@ function Profile() {
     }
   }
 
+  const onEdit = (listingId) => navigate(`/edit-listing/${listingId}`)
+
   return (
     <div className='profile'>
       <header className='profileHead'>
@@ -166,6 +168,7 @@ function Profile() {
                   listing={listing.data}
                   id={listing.id}
                   onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
