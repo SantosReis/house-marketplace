411555478244f793ec8f3e64f102853cdb7d6ad2
Marcus Reis <marcus.reis@askblue.pt>
2023-08-22 21:45:58 +0100

    Clear Up Console Warnings

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD3lGbGE3-aYukiyLvtz6A04P6DcDqWi2s",
-  authDomain: "house-marketplace-app-bf314.firebaseapp.com",
-  projectId: "house-marketplace-app-bf314",
-  storageBucket: "house-marketplace-app-bf314.appspot.com",
-  messagingSenderId: "218919058781",
-  appId: "1:218919058781:web:62dade97c2329d1917fcb4"
-};
+  apiKey: 'AIzaSyD3lGbGE3-aYukiyLvtz6A04P6DcDqWi2s',
+  authDomain: 'house-marketplace-app-bf314.firebaseapp.com',
+  projectId: 'house-marketplace-app-bf314',
+  storageBucket: 'house-marketplace-app-bf314.appspot.com',
+  messagingSenderId: '218919058781',
+  appId: '1:218919058781:web:62dade97c2329d1917fcb4',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig)
 export const db = getFirestore()
